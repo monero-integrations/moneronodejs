cbb3353735118601a931ef8bd6b06af1c35dbdaf
sneurlax <sneurlax@gmail.com>
2018-03-26 21:08:11 -0700

    example file and mocha test suite

--- example.js
+++ example.js
@@ -1,4 +1,20 @@
 var Monero = require('./index.js');
 
 var daemonRPC = new Monero.daemonRPC();
+// var daemonRPC = new Monero.daemonRPC('127.0.0.1', 28081, 'user', 'pass', 'http'); // Example of passing in parameters
+// var daemonRPC = new Monero.daemonRPC({ port: 28081, protocol: 'https'); // Parameters can be passed in as an object/dictionary
+
+daemonRPC.getblockcount()
+.then(height => {
+  console.log(height);
+});
+
 var walletRPC = new Monero.walletRPC();
+
+walletRPC.open_wallet()
+.then(wallet => {
+  walletRPC.getaddress()
+  .then(balance => {
+    console.log(balance);
+  });
+});
